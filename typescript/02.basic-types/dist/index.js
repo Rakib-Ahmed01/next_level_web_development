47683930e05e823886b4de715a9bffb5845710ba
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-01 02:21:34 +0600

    primitive types

--- typescript/02.basic-types/dist/index.js
+++ typescript/02.basic-types/dist/index.js
@@ -1,3 +1,17 @@
 "use strict";
 const course = 'Next Level Web Development Course';
 console.log(course);
+//            ^?
+let x = 'Unknown Type';
+// x.toPrecision(2); // we can not do it with unknown types
+let y = 'Any Type';
+// y.toPrecision(2); // we can do it with any types
+// Primitive types - number, string, boolean, null, undefined
+const number = 123;
+const nam = 'Rakib Ahmed';
+const isAdmin = false;
+const nullable = null;
+let notDefined = undefined;
+//   ^?
+const notDefined2 = undefined;
+//        ^?
--- typescript/02.basic-types/src/index.ts
+++ typescript/02.basic-types/src/index.ts
@@ -1,2 +1,18 @@
 const course: string = 'Next Level Web Development Course';
 console.log(course);
+//            ^?
+
+let x: unknown = 'Unknown Type';
+// x.toPrecision(2); // we can not do it with unknown types
+let y: any = 'Any Type';
+// y.toPrecision(2); // we can do it with any types
+
+// Primitive types - number, string, boolean, null, undefined
+const number: number = 123;
+const nam: string = 'Rakib Ahmed';
+const isAdmin: boolean = false;
+const nullable: null = null;
+let notDefined = undefined;
+//   ^?
+const notDefined2 = undefined;
+//        ^?
