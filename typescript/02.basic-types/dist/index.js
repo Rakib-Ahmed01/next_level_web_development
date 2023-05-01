f66d4886073c0b3474479e4503979fa5e4e7578f
Rakib-Ahmed01 <rakibahmed34360@gmail.com>
2023-05-01 15:37:30 +0600

    any vs unknown type

--- typescript/02.basic-types/dist/index.js
+++ typescript/02.basic-types/dist/index.js
@@ -21,5 +21,37 @@ const array = [];
 const array2 = [];
 const array3 = [1, 2, 3, 4, 5, 'str'];
 array3.push('str', 'str');
+const array4 = [{ id: 1, name: 'Rakib' }];
+const array5 = [
+    { id: 1, name: 'Rakib' },
+    { name: 'Sabbir', id: 2 },
+];
 // Tuples type
 const user = ['Rafin', 10, true];
+const student = {
+    name: 'Sabbir Ahmed',
+    roll: 1,
+    class: 5,
+};
+// student.class = 'five' // Cannot do this
+// student.phone = '019...'; // Error!
+// Literal type
+let changingString = 'Hello, world!';
+//    ^?
+changingString = 'Ola, mundo!';
+const unchangingString = 'Hello, world!';
+//      ^?
+let rakib = 'Rakib';
+//    ^?
+// Any Vs Unknown
+function anyFn(param) {
+    return param.toString(); // we can to this without knowing the type of the param
+    //       ^?
+}
+function unknownFn(param) {
+    // return param.toString(); // throws an error, toString might not be in param but if we chech the type we can do it
+    if (typeof param === 'number') {
+        return param.toString(); // narrowed the type to number
+        //        ^?
+    }
+}
--- typescript/02.basic-types/src/index.ts
+++ typescript/02.basic-types/src/index.ts
@@ -23,6 +23,62 @@ const array: string[] = [];
 const array2: Array<string> = [];
 const array3: (string | number)[] = [1, 2, 3, 4, 5, 'str'];
 array3.push('str', 'str');
+const array4: Array<{ name: string; id: number }> = [{ id: 1, name: 'Rakib' }];
+const array5: { name: string; id: number }[] = [
+  { id: 1, name: 'Rakib' },
+  { name: 'Sabbir', id: 2 },
+];
 
 // Tuples type
 const user: [string, number, boolean] = ['Rafin', 10, true];
+
+// Object type
+interface IStudent {
+  name: string;
+  roll: number;
+  class: number;
+}
+
+type TStudent = {
+  name: string;
+  roll: number;
+  class: number;
+};
+
+const student: {
+  name: string;
+  roll: number;
+  class: number;
+} = {
+  name: 'Sabbir Ahmed',
+  roll: 1,
+  class: 5,
+};
+
+// student.class = 'five' // Cannot do this
+// student.phone = '019...'; // Error!
+
+// Literal type
+let changingString = 'Hello, world!';
+//    ^?
+changingString = 'Ola, mundo!';
+
+const unchangingString = 'Hello, world!';
+//      ^?
+
+let rakib: 'Rakib' = 'Rakib';
+//    ^?
+
+// Any Vs Unknown
+function anyFn(param: any) {
+  return param.toString(); // we can to this without knowing the type of the param
+  //       ^?
+}
+
+function unknownFn(param: unknown) {
+  // return param.toString(); // throws an error, toString might not be in param but if we chech the type we can do it
+  if (typeof param === 'number') {
+    return param.toString(); // narrowed the type to number
+    //        ^?
+  }
+}
